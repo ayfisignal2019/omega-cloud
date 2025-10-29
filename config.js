46bcc2aef32a9e043ca47d05ccd5400a2ca3d1df
ayfisignal2019 <izaizai1985@gmail.com>
2025-10-29 08:32:43 +0330

    Create config.js for sensitive information
    Added configuration file with sensitive tokens and credentials.

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js — حاوی توکن‌ها و اطلاعات حساس
+module.exports = {
+  TG_TOKEN: "8229664295:AAHwzrJ4nKbHYoNN2ju8WSFUdaOqOVlmAh4",  // توکن ربات تلگرام شما
+  TG_CHAT: "85026023",                                           // چت آیدی شما (کاربر شخصی)
+  GMAIL_USER: "izaizai1985@gmail.com",                           // ایمیل جیمیل شما
+  GMAIL_PASS: "qifxnxnxvcnyflyw"                                 // App Password (درست و فعال)
+};
